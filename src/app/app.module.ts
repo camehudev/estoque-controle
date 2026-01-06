4213953b372e24572b6c7908b62a39df2a06d10e
Carlos Humberto <camehudev@gmail.com>
2026-01-06 11:13:28 -0300

    criado pages: Home,Login

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -6,21 +6,33 @@ import { AppComponent } from './app.component';
 import { provideAnimationsAsync } from '@angular/platform-browser/animations/async';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import {MatToolbarModule} from '@angular/material/toolbar';
+import { LoginComponent } from './pages/login/login.component';
+import { MenuBarComponent } from './components/menu-bar/menu-bar.component';
+import { HomeComponent } from './pages/home/home.component';
+import {MatCardModule} from '@angular/material/card';
+
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    LoginComponent,
+    MenuBarComponent,
+    HomeComponent
+    
   ],
   imports: [
     BrowserModule,
     AppRoutingModule,
     BrowserAnimationsModule,
-    MatToolbarModule
+    MatToolbarModule,
+    MatCardModule,
+    
   ],
   providers: [
     provideClientHydration(),
     provideAnimationsAsync()
   ],
+  
   bootstrap: [AppComponent]
 })
 export class AppModule { }
